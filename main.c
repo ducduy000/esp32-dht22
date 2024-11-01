c263551c2a6a263ac84b4bf2d4045fb4ec69243f
ducduy000 <167493730+ducduy000@users.noreply.github.com>
2024-11-01 23:28:15 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> //Requires by memset
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include <esp_http_server.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "freertos/event_groups.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "driver/gpio.h"
+#include <lwip/sockets.h>
+#include <lwip/sys.h>
+#include <lwip/api.h>
+#include <lwip/netdb.h>
+#include "DHT22.h"
+#define EXAMPLE_ESP_WIFI_SSID      "myssid"
+#define EXAMPLE_ESP_WIFI_PASS      "mypassword"
+#define EXAMPLE_ESP_WIFI_CHANNEL   1
+#define EXAMPLE_MAX_STA_CONN       4
+//#define MIN(x,y) ((x) <(y) ? (x) : (y))
+//#define EXAMPLE_ESP_MAXIMUM_RETRY 10
+
+static const char *TAG = "esp32 webserver";
+
+/* FreeRTOS event group to signal when we are connected*/
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+
+//static int s_retry_num = 0;
+
+
+double temp;
+double hum;
+
+char html_page[] = "<!DOCTYPE HTML><html>\n"
+                   "<head>\n"
+                   "  <title>ESP-IDF DHT22 Web Server</title>\n"
+                   "  <meta http-equiv=\"refresh\" content=\"10\">\n"
+                   "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+                   "  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\" integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">\n"
+                   "  <link rel=\"icon\" href=\"data:,\">\n"
+                   "  <style>\n"
+                   "    html {font-family: Arial; display: inline-block; text-align: center;}\n"
+                   "    p {  font-size: 1.2rem;}\n"
+                   "    body {  margin: 0;}\n"
+                   "    .topnav { overflow: hidden; background-color: #241d4b; color: white; font-size: 1.7rem; }\n"
+                   "    .content { padding: 20px; }\n"
+                   "    .card { background-color: white; box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5); }\n"
+                   "    .cards { max-width: 700px; margin: 0 auto; display: grid; grid-gap: 2rem; grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); }\n"
+                   "    .reading { font-size: 2.8rem; }\n"
+                   "    .card.temperature { color: #0e7c7b; }\n"
+                   "    .card.humidity { color: #17bebb; }\n"
+                   "  </style>\n"
+                   "</head>\n"
+                   "<body>\n"
+                   "  <div class=\"topnav\">\n"
+                   "    <h3>ESP-IDF DHT22 WEB SERVER</h3>\n"
+                   "  </div>\n"
+                   "  <div class=\"content\">\n"
+                   "    <div class=\"cards\">\n"
+                   "      <div class=\"card temperature\">\n"
+                   "        <h4><i class=\"fas fa-thermometer-half\"></i> TEMPERATURE</h4><p><span class=\"reading\">%.2f&deg;C</span></p>\n"
+                   "      </div>\n"
+                   "      <div class=\"card humidity\">\n"
+                   "        <h4><i class=\"fas fa-tint\"></i> HUMIDITY</h4><p><span class=\"reading\">%.2f</span> &percnt;</span></p>\n"
+                   "      </div>\n"
+                   "    </div>\n"
+                   "  </div>\n"
+                   "</body>\n"
+                   "</html>";
+
+void DHT_readings()
+{
+	setDHTgpio(GPIO_NUM_4);
+	
+	int ret = readDHT();
+		
+	errorHandler(ret);
+
+    temp = getTemperature();
+    hum = getHumidity();
+
+    vTaskDelay(2000 / portTICK_RATE_MS);
+		
+}
+ 
+ 
+ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                    int32_t event_id, void* event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+                 
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+                
+    }
+}
+
+void wifi_init_softap(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        NULL));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
+            .channel = EXAMPLE_ESP_WIFI_CHANNEL,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            .max_connection = EXAMPLE_MAX_STA_CONN,
+#ifdef CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT
+            .authmode = WIFI_AUTH_WPA3_PSK,
+            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+#else /* CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT */
+            .authmode = WIFI_AUTH_WPA2_PSK,
+#endif
+            .pmf_cfg = {
+                    .required = true,
+            },
+        },
+    };
+    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
+             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_WIFI_CHANNEL);
+}
+
+esp_err_t send_web_page(httpd_req_t *req)
+{
+    int response;
+    DHT_readings();
+    char response_data[sizeof(html_page) + 50];
+    memset(response_data, 0, sizeof(response_data));
+    sprintf(response_data, html_page, temp, hum);
+    response = httpd_resp_send(req, response_data, HTTPD_RESP_USE_STRLEN);
+
+    return response;
+}
+
+esp_err_t get_req_handler(httpd_req_t *req)
+{
+    return send_web_page(req);
+}
+
+httpd_uri_t uri_get = {
+    .uri = "/get",
+    .method = HTTP_GET,
+    .handler = get_req_handler,
+    .user_ctx = NULL};
+
+httpd_handle_t setup_server(void)
+{
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t server = NULL;
+
+    if (httpd_start(&server, &config) == ESP_OK)
+    {
+        httpd_register_uri_handler(server, &uri_get);
+    }
+
+    return server;
+}
+
+void app_main()
+{
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_SoftAP");
+    wifi_init_softap();
+    setup_server();
+    
+} 
